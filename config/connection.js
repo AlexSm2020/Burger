7082ddfdfab8ae10c2cb113db11e5593c50f3384
Aref Smiley <aref.smiley@yahoo.com>
2020-05-17 15:15:55 -0400

    5TH draft

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,7 @@
 const mysql = require("mysql");
 var connection;
+require("dotenv").config();
+
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
