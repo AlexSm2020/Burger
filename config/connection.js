ec527f0cafbdc6b102d530397fb3ae0b5520b5e2
Aref Smiley <aref.smiley@yahoo.com>
2020-05-17 12:29:21 -0400

    3rd Draft

--- config/connection.js
+++ config/connection.js
@@ -5,6 +5,7 @@ if (process.env.JAWSDB_URL) {
 } else {
   connection = mysql.createConnection({
     host: "localhost",
+    port: 3306,
     user: "root",
     password: "Pogo131433$",
     database: "burgers_db"
